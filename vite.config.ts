ad8946cd6ac56e1bdd516764096c90bb0f63b8ae
Franco Miceli <francomiceli.94@gmail.com>
2026-01-12 22:41:07 -0300

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,12 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+    build: {
+    rollupOptions: {
+      external: ['@solana-program/system'],
+    },
+  },
+  optimizeDeps: {
+    exclude: ['@solana-program/system'],
+  },
 }));
